c96010ce9a7333a26b9349ac03127d45efcf3527
Daniel Way <projects@danieldway.com>
2020-09-22 11:50:07 -0500

    feat(globusflow): add logic from app.java

--- src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/FBASeleniumGlobusAuthenticationFlow.java
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/FBASeleniumGlobusAuthenticationFlow.java
@@ -1,6 +1,9 @@
 package edu.iastate.ece.sd.sdmay2126.runner.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.annotation.Nonnull;
 
@@ -16,6 +19,55 @@ public class FBASeleniumGlobusAuthenticationFlow implements SeleniumAuthenticati
 
     @Override
     public void authenticateSession() {
-        // TODO
+        System.out.println("Initiating Globus authentication...");
+
+        // Locate the login iframe
+        WebElement loginFrame = new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.tagName("iframe")));
+
+        // Swap the driver context into the frame
+        driver.switchTo().frame(loginFrame);
+
+        // Locate and click the Globus auth
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElements(By.className("signin-button")))
+                .get(2) // Globus is #3
+                .click();
+
+        // Switch back to the window (from the iframe)
+        driver.switchTo().defaultContent();
+
+        // Locate the Globus "continue" button
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.name("identity_provider")))
+                .click();
+
+        // Locate and fill the username and password fields
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.id("ember24")))
+                .sendKeys("sdmay2126");
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.id("ember25")))
+                .sendKeys("sdmay2126pw");
+
+        // Submit the login form
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.cssSelector("input[type=\"submit\"]")))
+                .click();
+
+        // Locate the confirmation iframe
+        WebElement confirmFrame = new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.tagName("iframe")));
+
+        // Swap the driver context into the frame
+        driver.switchTo().frame(confirmFrame);
+
+        // Acknowledge account
+        new WebDriverWait(driver, 10)
+                .until(d -> d.findElement(By.tagName("button")))
+                .click();
+
+        // Swap back to the window
+        driver.switchTo().defaultContent();
     }
 }
