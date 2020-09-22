15702c0c6e663831f83541d3a517c28a08d2f5aa
Daniel Way <projects@danieldway.com>
2020-09-22 12:28:15 -0500

    feat(seleniumconfig): add narrative id

--- src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/SeleniumConfiguration.java
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/SeleniumConfiguration.java
@@ -11,19 +11,29 @@ import edu.iastate.ece.sd.sdmay2126.runner.selenium.driver.SeleniumDrivers;
 public class SeleniumConfiguration {
     private SeleniumDriverConfiguration driverConfiguration;
     private SeleniumAuthenticationConfiguration authenticationConfiguration;
+    private int narrativeIdentifier;
 
     // TODO: This little "helper" should probably be removed/refactored to decouple from Globus
-    public SeleniumConfiguration(String globusUsername, String globusPassword, SeleniumDrivers driverType, String driverLocation) {
+    public SeleniumConfiguration(
+            String globusUsername, String globusPassword, SeleniumDrivers driverType,
+            String driverLocation, int narrativeIdentifier) {
         this(
                 new SeleniumDriverConfiguration(driverType, driverLocation),
-                new GlobusAuthenticationConfiguration(globusUsername, globusPassword)
+                new GlobusAuthenticationConfiguration(globusUsername, globusPassword),
+                narrativeIdentifier
         );
     }
 
+    /**
+     * @param narrativeIdentifier The final URL path segment which identifies the narrative to load.
+     */
     public SeleniumConfiguration(
             SeleniumDriverConfiguration driverConfiguration,
-            SeleniumAuthenticationConfiguration authenticationConfiguration) {
+            SeleniumAuthenticationConfiguration authenticationConfiguration,
+            int narrativeIdentifier) {
         this.driverConfiguration = driverConfiguration;
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.narrativeIdentifier = narrativeIdentifier;
     }
 
     public SeleniumDriverConfiguration getDriverConfiguration() {
@@ -32,5 +42,9 @@ public class SeleniumConfiguration {
 
     public SeleniumAuthenticationConfiguration getAuthenticationConfiguration() {
         return authenticationConfiguration;
+    }
+
+    public int getNarrativeIdentifier() {
+        return narrativeIdentifier;
     }
 }
