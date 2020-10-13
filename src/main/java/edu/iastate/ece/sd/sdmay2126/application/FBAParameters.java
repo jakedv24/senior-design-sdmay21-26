b141bc5b3a38b199bf512dea19687f44942194ab
PukTheGreat <PukTheGreat@gmail.com>
2020-10-13 15:03:37 -0500

    Fixed Merge conflicts, and added GUIForm updates

--- src/main/java/edu/iastate/ece/sd/sdmay2126/application/FBAParameters.java
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/application/FBAParameters.java
@@ -15,6 +15,7 @@ public class FBAParameters implements ApplicationParameters {
     private String reactionToMaximize;
     private float expressionThreshold;
     private float expressionUncertainty;
+    private String geneKnockouts;
     // TODO geneKnockouts
     // TODO reactionKnockouts
     // TODO customFluxBounds
@@ -115,6 +116,14 @@ public class FBAParameters implements ApplicationParameters {
 
     public String getReactionToMaximize() {
         return  reactionToMaximize;
+    }
+
+    public String getGeneKnockouts() {
+        return geneKnockouts;
+    }
+
+    public void setGeneKnockouts(String geneKnockouts) {
+        this.geneKnockouts = geneKnockouts;
     }
 
     @Override
