eccf9f9685acfa8541a2afc4b9180e8237fa7a1e
PukTheGreat <PukTheGreat@gmail.com>
2020-10-13 15:32:59 -0500

    Added GeneKnockout clicking button and sending key

--- src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/FBASeleniumInputProgrammer.java
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/FBASeleniumInputProgrammer.java
@@ -107,6 +107,18 @@ public class FBASeleniumInputProgrammer implements SeleniumInputProgrammer {
         }
         foundRMItem.get(0).click();
 
+        System.out.println("Setting GeneKnockouts");
+        WebElement geneKnockouts = scopedFBACard
+                .findElement(By.cssSelector("div[data-parameter='feature_ko_list']"));
+        WebElement geneKnockoutsButton = geneKnockouts
+                .findElement(By.cssSelector("button[class='btn btn-default']"));
+            geneKnockoutsButton.click();
+
+        WebElement geneKnockoutsText = geneKnockouts
+                .findElement(By.cssSelector("input[class='form-control']"));
+        geneKnockoutsText.clear();
+        geneKnockoutsText.sendKeys(params.getGeneKnockouts());
+
 
         System.out.println("Programming FBA complete.");
     }
