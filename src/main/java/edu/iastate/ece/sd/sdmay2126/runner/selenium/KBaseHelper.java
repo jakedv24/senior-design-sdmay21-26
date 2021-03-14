a993ff2227ccf28969baa565a08c25307955d367
Daniel Way <contact@danieldway.com>
2021-03-14 18:45:48 -0500

    feat(kbasehelper): implement setcheckbox

--- src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/KBaseHelper.java
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/selenium/KBaseHelper.java
@@ -1,5 +1,8 @@
 package edu.iastate.ece.sd.sdmay2126.runner.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * Automation routines for KBase which are not application-specific.
  *
@@ -13,8 +16,22 @@ package edu.iastate.ece.sd.sdmay2126.runner.selenium;
 public class KBaseHelper {
     /**
      * Sets the value of a checkbox input.
+     *
+     * @param codeCell The KBase code cell encompassing the checkbox.
+     * @param parameterName The value of "data-parameter" in the surrounding div.
+     * @param value The new checkbox value (true checked, false unchecked).
      */
-    public static void setCheckBox() {}
+    public static void setCheckBox(WebElement codeCell, String parameterName, boolean value) {
+        // Checkbox inputs are wrapped by a div which includes an identifier for the particular parameter
+        WebElement checkbox = codeCell
+                .findElement(By.cssSelector("div[data-parameter='" + parameterName + "']"))
+                .findElement(By.cssSelector("input[type='checkbox']"));
+
+        // If the checkbox doesn't match the set value, toggle it
+        if (checkbox.isSelected() != value) {
+            checkbox.click();
+        }
+    }
 
     /**
      * Sets the value of a single-value text input.
