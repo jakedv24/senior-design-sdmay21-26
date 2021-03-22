4077fae232c194d19584bcee5431a0d19f3fe49a
clmeyer <clmeyer@iastate.edu>
2021-03-22 12:06:35 -0500

    added SeleniumResultsCardDeleter and FBAResultsCardDeleter

---
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/gui/application/fluxbalanceanalysis/FBAResultsCardDeleter.java
@@ -0,0 +1,12 @@
+package edu.iastate.ece.sd.sdmay2126.runner.gui.application.fluxbalanceanalysis;
+
+import edu.iastate.ece.sd.sdmay2126.orchestration.Job;
+import edu.iastate.ece.sd.sdmay2126.runner.gui.selenium.SeleniumResultsCardDeleter;
+import org.openqa.selenium.WebDriver;
+
+public class FBAResultsCardDeleter implements SeleniumResultsCardDeleter {
+    @Override
+    public void DeleteResultsCard(Job job, WebDriver driver) {
+
+    }
+}
---
+++ src/main/java/edu/iastate/ece/sd/sdmay2126/runner/gui/selenium/SeleniumResultsCardDeleter.java
@@ -0,0 +1,9 @@
+package edu.iastate.ece.sd.sdmay2126.runner.gui.selenium;
+
+import edu.iastate.ece.sd.sdmay2126.orchestration.Job;
+import org.openqa.selenium.WebDriver;
+
+
+public interface SeleniumResultsCardDeleter {
+    void DeleteResultsCard(Job job, WebDriver driver);
+}
